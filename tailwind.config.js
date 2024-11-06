b5f011b66fa027189ea81cb14f9cfc7219328d28
Mamunur-Roshid99 <roshidmamunur569@gmail.com>
2024-11-07 00:42:36 +0600

    practice project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      robo: ["Roboto", "sans-serif"],
+    }
+  },
+  plugins: [],
+};
